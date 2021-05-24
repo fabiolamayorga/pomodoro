c3a19cff00c9e4eb6ccc3e110b59287d7775ea3b
Fabiola <fabiola.mayorga.con@greatminds.org>
2021-05-24 10:35:32 -0600

    small update on text

--- src/App.js
+++ src/App.js
@@ -32,29 +32,13 @@ const App = () => {
       // });
       setPomodorosCounter(pomodorosCounter + 1);
     }
-    // let year = new Date().getFullYear();
-    // const difference = +new Date(`10/01/${year}`) - +new Date();
 
-    // let timeLeft = {};
-
-    // if (difference > 0) {
-    //   timeLeft = {
-    //     minutes: Math.floor((difference / 1000 / 60) % 60),
-    //     seconds: Math.floor((difference / 1000) % 60)
-    //   };
-    // }
-
-
-    // useEffect(() => {
-    //   const date = new Date();
-    //   console.log(date);
-    // });
 
     return (
       <div className="pomodoro">
         <h1>Pomodoro</h1>
         <Timer sessionTime = {sessionTime} pomodorosCounter={handlePomodorosCounter}/>
-        <p>Sessions Completed: <b>{pomodorosCounter}</b></p>
+        <p>Pomodoros Completed: <b>{pomodorosCounter}</b></p>
         <button>Pause</button>
         <button>Long Break</button>
         <button>Short Break</button>
